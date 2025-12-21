85af78ed299c1f2ccfa3d152e481cfd5b2344a2c
Sanyam Panchal <sanyam9922@gmail.com>
2025-12-21 23:27:39 +0530

    Update attention_unet_training.py

--- attention_unet_training.py
+++ attention_unet_training.py
@@ -168,7 +168,7 @@ val_transforms = Compose([
 ])
 
 train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate=1.0, num_workers=4)
-train_loader = DataLoader(train_ds, batch_size=2, shuffle=True, num_workers=4)
+train_loader = DataLoader(train_ds, batch_size=4, shuffle=True, num_workers=4)
 
 val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=4)
 val_loader = DataLoader(val_ds, batch_size=1, num_workers=4)
