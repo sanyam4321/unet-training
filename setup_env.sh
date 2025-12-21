53dd78093879d3e291962e788b8590fe0cc0bb74
sanyam9922 <sanyam9922@gmail.com>
2025-12-21 23:49:09 +0530

    disk

--- setup_env.sh
+++ setup_env.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 # 1. Copy the dataset to /home
-echo "--- Copying Dataset ---"
-if [ -d "/home/Task10_Colon_Preprocessed" ]; then
-    echo "Dataset already exists in /home, skipping copy."
-else
-    # Using -r for recursive and -p to preserve timestamps/permissions
-    cp -rp /workspace/Task10_Colon_Preprocessed /home/
-    echo "Dataset copied successfully."
-fi
+# echo "--- Copying Dataset ---"
+# if [ -d "/home/Task10_Colon_Preprocessed" ]; then
+#     echo "Dataset already exists in /home, skipping copy."
+# else
+#     # Using -r for recursive and -p to preserve timestamps/permissions
+#     cp -rp /workspace/Task10_Colon_Preprocessed /home/
+#     echo "Dataset copied successfully."
+# fi
+
+# 1. Create a folder for the mount point
+sudo mkdir -p /mnt/ramdisk
+
+sudo mount -t tmpfs -o size=40G tmpfs /mnt/ramdisk
+
+sudo rsync -av --progress /workspace/Task10_Colon_Preprocessed/ /mnt/ramdisk/
+
+sudo chmod -R 777 /mnt/ramdisk
 
 # 2. Install nano and essential libraries
 echo "--- Installing Nano and System Dependencies ---"
