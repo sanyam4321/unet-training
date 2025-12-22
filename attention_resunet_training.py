dbe67b8b5d150ed662c19b63dd288c45054b8db7
sanyam9922 <sanyam9922@gmail.com>
2025-12-22 15:54:25 +0530

    params

--- attention_resunet_training.py
+++ attention_resunet_training.py
@@ -177,11 +177,11 @@ val_transforms = Compose([
     EnsureTyped(keys=["image", "label"]),
 ])
 
-train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate=1.0, num_workers=4)
-train_loader = DataLoader(train_ds, batch_size=3, shuffle=True, num_workers=4)
+train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate=1.0, num_workers=6)
+train_loader = DataLoader(train_ds, batch_size=3, shuffle=True, num_workers=6)
 
-val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=4)
-val_loader = DataLoader(val_ds, batch_size=1, num_workers=4)
+val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=6)
+val_loader = DataLoader(val_ds, batch_size=1, num_workers=6)
 
 
 max_epochs = 100
